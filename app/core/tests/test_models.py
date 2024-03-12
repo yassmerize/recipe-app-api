f0db9536999d378977734adfc49f2e59c02bb201
yassmerize <yassinelahouir89@gmail.com>
2024-03-12 16:47:00 +0100

    User API test fix bug

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,7 +4,7 @@ Test for models.
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-class TestModels(TestCase):
+class ModelsTest(TestCase):
     """ Test models."""
 
     def test_create_user_with_email_successful(self):
